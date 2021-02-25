25bf3a7d48093ec538d2fee00b6c7155d3b45a89
ext.agnieszkal <agnieszka.liszka@fitech.pl>
2021-02-25 14:39:50 +0100

    Change date format

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -2,14 +2,15 @@
   <div class="currencyConverter">
     <el-form label-position="top" label-width="130px">
       <el-form-item label="Currency converter">
-        <el-date-picker v-model="date" type="date" format="yyyy-MM-DD">
+        <el-date-picker v-model="date" type="date" format="yyyy-MM-dd">
         </el-date-picker>
       </el-form-item>
     </el-form>
-    <button>Get GBP currency rate</button>
-    <button>Get EUR currency rate</button>
-    <button>Get CHF currency rate</button>
-    <button>Get USD currency rate</button>
+    <button @click="getGBPexchangeRate()">Get GBP currency rate</button>
+    <button @click="getEURexchangeRate()">Get EUR currency rate</button>
+    <button @click="getCHFexchangeRate()">Get CHF currency rate</button>
+    <button @click="getUSDexchangeRate()">Get USD currency rate</button>
+    <button @click="test()">Test</button>
   </div>
 </template>
 <script lang="ts">
@@ -21,6 +22,7 @@ export default class HelloWorld extends Vue {
   date: string = "";
 
   getGBPexchangeRate(): void {
+    console.log(this.date);
     const gbpExchangeRates = axios.get(
       `http://api.nbp.pl/api/exchangerates/rates/a/gbp/${this.date}/?format=json`
     );
@@ -43,6 +45,12 @@ export default class HelloWorld extends Vue {
       `http://api.nbp.pl/api/exchangerates/rates/a/usd/${this.date}/?format=json`
     );
     console.log(usdExchangeRates);
+  }
+  test(): void {
+    const test = axios.get(
+      "http://api.nbp.pl/api/exchangerates/rates/c/usd/2016-04-04/?format=json"
+    );
+    console.log(test);
   }
 }
 </script>
