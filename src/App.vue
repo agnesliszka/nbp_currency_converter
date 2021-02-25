fe77d1ae33afe69fdeea57da0b83c797ec91bd17
ext.agnieszkal <agnieszka.liszka@fitech.pl>
2021-02-25 14:05:34 +0100

    Start nbp currency converter project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <CurrencyConverter />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import CurrencyConverter from "./components/CurrencyConverter.vue";
+
+@Component({
+  components: {
+    CurrencyConverter,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CurrencyConverter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="currencyConverter">
+    <div>Currency converter</div>
+    <el-form label-position="left" label-width="130px">
+      <el-form-item label="`Currency converter">
+        <el-date-picker v-model="date" type="date" format="dd-MMM-yyyy">
+        </el-date-picker>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  date: string = "";
+}
+</script>
+<style scoped></style>
